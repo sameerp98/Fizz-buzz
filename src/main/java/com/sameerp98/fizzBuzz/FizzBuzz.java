7b7bfbfa78e7d90f32097a50eac631b293c01132
Sameer Padalia <sameerp98@pop-os.localdomain>
2020-06-13 17:48:56 +0530

    ENHANCE: added multiples of 3 and 5

--- src/main/java/com/sameerp98/fizzBuzz/FizzBuzz.java
+++ src/main/java/com/sameerp98/fizzBuzz/FizzBuzz.java
@@ -3,6 +3,9 @@ package com.sameerp98.fizzBuzz;
 public class FizzBuzz {
 
     public static String convertToFizzBuzz(int i) {
+        if (i % 15 == 0) {
+            return "FizzBuzz";
+        }
         if (i % 3 == 0) {
             return "Fizz";
         }
