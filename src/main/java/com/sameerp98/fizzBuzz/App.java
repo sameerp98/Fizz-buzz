62e23f279dde2ad2b7184a6bf569b86943b5c3d6
Sameer Padalia <sameerp98@pop-os.localdomain>
2020-06-13 13:10:30 +0530

    DOC:created readme

---
+++ src/main/java/com/sameerp98/fizzBuzz/App.java
@@ -0,0 +1,13 @@
+package com.sameerp98.fizzBuzz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
